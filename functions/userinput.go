89ad49fbfa2f622fa80ce4a19627d88dbb552b3a
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-05-18 23:04:07 +0200

    Added the clearscreen function to clear screen

--- functions/userinput.go
+++ functions/userinput.go
@@ -4,10 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"strconv"
 	"strings"
 )
 
+func clearScreen() {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
 func UserInput(prompt string) string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(prompt)
