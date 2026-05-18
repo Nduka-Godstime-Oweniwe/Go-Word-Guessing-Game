71a759c63b9874f6fb223fa4264ed648551fe80a
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-05-18 23:21:44 +0200

    added the how to play instructions

--- main.go
+++ main.go
@@ -44,6 +44,13 @@ func main() {
 
 		} else if option == 3 {
 			fmt.Println("How To Play")
+			fmt.Println("1. A word would be shown on the screen")
+			fmt.Println("2. The letters of the word would be rearranged")
+			fmt.Println("3. You are to guess the word")
+			fmt.Println("4. The number of tries you have depends on the difficulty level")
+			fmt.Println("\tA. Easy >> 5 tries\n\tB. Medium >> 3 tries\n\tC. Hard >> 1 try only")
+			fmt.Println("5. The points you get if you guess correctly depends on the difficulty level")
+			fmt.Println("\tA. Easy >> 1 point\n\tB. Medium >> 3 points\n\tC. Hard >> 5 points")
 		} else {
 			break
 		}
